1f1a889f0f0984eb3a18a0fce8d7d4265bbb8045
Ильинский Денис Романович <vazdenservis@gmail.com>
2023-03-15 16:39:05 +0000

    Merged in Initializatoin/ITMPJ-276 (pull request #1)
    1
    
    * added a class to launch the service
    
    * 1
    
    
    Approved-by: Руслан Захаров

---
+++ src/main/java/com/itm/advice/advisementservice/AdviceAdvisementApplication.java
@@ -0,0 +1,11 @@
+package com.itm.advice.advisementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceAdvisementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdviceAdvisementApplication.class, args);
+    }
+}
